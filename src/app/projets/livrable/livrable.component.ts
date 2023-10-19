c7568dcd10c120af31808367a63608829d4ec499
dardour Med <dardour.mohammed@gmail.com>
2023-10-19 11:08:26 +0200

    modification composant avis et affectation sujet

--- src/app/projets/livrable/livrable.component.ts
+++ src/app/projets/livrable/livrable.component.ts
@@ -1,10 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
 import { Router } from '@angular/router';
-import { Observable, catchError, throwError } from 'rxjs';
+import { NgbAlertConfig } from '@ng-bootstrap/ng-bootstrap';
+import { Observable, catchError, interval, throwError } from 'rxjs';
+import { avis } from 'src/app/model/avis';
 import { Livrable } from 'src/app/model/livrable';
 import { AuthService } from 'src/app/services/auth.service';
+import { AvisService } from 'src/app/services/avis.service';
 import { LivrableService } from 'src/app/services/livrable.service';
+import { UserService } from 'src/app/services/user.service';
 
 @Component({
   selector: 'app-livrable',
@@ -16,15 +20,32 @@ export class LivrableComponent implements OnInit {
   livrables2!: any;
   errorMessage!: string;
   searchFormGroup: FormGroup | undefined;
+  avis!:avis[];
+  idLivrable!:number;
+  avisAjout!:avis;
+  affichageAvis=false;
+  showAlert = false;
 
   constructor(
     private livrableService: LivrableService,
     public authService: AuthService,
     private router: Router,
-    private fb: FormBuilder
-  ) {}
+    private fb: FormBuilder,
+    private userService:UserService,
+    private avisService:AvisService,
+    config: NgbAlertConfig
+  ) {
+    config.dismissible = true;
+    config.type = 'success';
+  }
 
   ngOnInit(): void {
+    this.avis = new Array();
+    this.idLivrable=0;
+    this.avisAjout=new avis();
+    this.userService.get(this.authService.username).subscribe(
+      response=>this.avisAjout.utilisateur=response
+    )
     this.searchFormGroup = this.fb.group({
       keyword: this.fb.control(''),
     });
@@ -67,5 +88,64 @@ export class LivrableComponent implements OnInit {
       .subscribe((data) => {
         this.livrables2 = data;
       });
+  }
+
+  afficherAvis(l:Livrable)
+  {
+    if(this.affichageAvis==false)
+    {
+      this.affichageAvis=true;
+      //this.avis=l.avis;
+      this.getAvisById(l.id);
+    }
+    else
+    {
+      this.affichageAvis=false;
+    }
+   
+  }
+
+  AjouterAvis(id:number)
+  {
+    if(this.idLivrable!=0)
+    {
+      this.idLivrable=0;
+    }
+    else
+    {
+    this.idLivrable=id;
+    //console.log(this.authService.username)
+  }
+    
+  }
+
+  validerAjout()
+  {
+    console.log(this.avisAjout.texteAvis)
+    console.log(this.avisAjout.utilisateur.userId)
+    this.avisAjout.livrable=new Livrable();
+   this.avisAjout.livrable.id=this.idLivrable;
+   this.avisService.addAvis(this.avisAjout).subscribe(
+    response=>{console.log("ajout ok")
+    this.idLivrable=0
+    //this.avis.push(response)
+    this.getAvisById(this.idLivrable);
+    this.avisAjout=new avis();
+    this.showAlert = true;
+    interval(3000).subscribe(() => {
+      this.showAlert = false;
+    })
+
+  }
+   )
+  }
+
+  getAvisById(id:number)
+  {
+    this.avisService.getAvisById(id).subscribe(
+      response=>{
+        this.avis=response;
+      }
+    )
   }
 }
